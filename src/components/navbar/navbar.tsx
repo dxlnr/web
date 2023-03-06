526cc2b8b62d6ca20606f3a02e9d6d096bc17c7c
dxlnr <daniel.illner@outlook.de>
2023-03-06 23:38:59 +0100

    tailwindcss in place

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,12 +2,19 @@ import { Routes, Route, A } from "@solidjs/router";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white relative z-10 bg-opacity-0">
-      <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="hidden md:block">
-          <div className="flex items-baseline space-x-4 md:gap-16 gap-0.5 text-base tracking-widest ml-10">
-            <A href="/about">about</A>
-            <A href="/blog">blog</A>
+    <nav class="bg-white relative z-10 bg-opacity-0">
+      <div class="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-between h-20">
+          <div class="w-full flex items-center justify-between">
+            <div class="flex-shrink-0">
+              <A href="/">home</A>
+            </div>
+            <div class="hidden md:block">
+              <div class="flex items-baseline space-x-4 md:gap-16 gap-0.5 text-base tracking-widest ml-10">
+                <A href="/about">about</A>
+                <A href="/blog">blog</A>
+              </div>
+            </div>
           </div>
         </div>
       </div>
