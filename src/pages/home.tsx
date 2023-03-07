a0cb7ca659fb0088ecd6106eb4f90d991dce9919
dxlnr <daniel.illner@outlook.de>
2023-03-07 01:26:41 +0100

    added footer

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,11 @@
+import Footer from "../components/footer/footer";
+
 const Home: Component = () => {
   return (
-    <>
+    <div class="p-10">
       <h1>Home</h1>
-    </>
+      <Footer />
+    </div>
   );
 };
 
